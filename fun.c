73ed5f8bd8c8c25fbd688d020d97f59b9b8fbcd7
Alexander Auradnik <alexander.auradnik@student.tgm.ac.at>
2012-12-06 11:16:44 +0100

    header verknüpfung

---
+++ fun.c
@@ -0,0 +1,10 @@
+#include "hilfe.h"
+
+int add_it(int i1, int i2){
+return i1+i2; 
+}
+
+int subtract_it(int i1, int i2){
+return i1-i2;
+}
+
---
+++ hilfe.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int add_it(int a, int b); 
+int subtract_it(int a, int b); 
+
